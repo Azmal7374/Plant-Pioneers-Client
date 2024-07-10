b81d7e83a7575e740bc8aab20c1eabea8b520b8e
Azmal <azmalgazi8@gmail.com>
2024-07-10 18:10:01 +0600

    navbar completed and update not found page"

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,14 +2,20 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../pages/share/Navbar";
 import Footer from "../pages/share/Footer";
 
-const MainLayout = () => {
-    return (
-        <div>
-          <Navbar/>
-          <Outlet/>
-          <Footer/>
-        </div>
-    );
+const Layout = () => {
+  return (
+    <div>
+      <div className="hidden lg:block bg-[#1B3048] text-white text-[12px] p-2">
+        <h3 className="text-center">10% OFF & FREE SHIPPING OVER $199! CODE: SUMMER10</h3>
+      </div>
+      <Navbar />
+      <div className="block lg:hidden bg-[#1B3048] text-white text-[12px] p-2">
+        <h3 className="text-center">30% OFF & FREE SHIPPING OVER $299! CODE: SUMMER10</h3>
+      </div>
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
-export default MainLayout;
+export default Layout;
--- src/pages/share/Navbar.tsx
+++ src/pages/share/Navbar.tsx
@@ -1,9 +1,8 @@
-import { NavLink } from "react-router-dom";
-// import logo from "../assets/logo.png";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { Link, NavLink } from "react-router-dom";
 import { Badge, Drawer } from "@material-tailwind/react";
 import { useState } from "react";
-import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { CiShoppingCart } from "react-icons/ci";
+import { IoIosMenu } from "react-icons/io";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -12,23 +11,27 @@ const Navbar = () => {
   const closeDrawer = () => setOpen(false);
 
   return (
-    <div className="flex justify-between items-center bg-[#D6EFD8] py-3 px-10 md:px-10 lg:px-20 shadow-xl sticky top-0 z-10">
-      <img src={""} className="w-[120px] md:w-[170px] lg:w-[170px]" />
-
-      <div className="hidden md:hidden lg:flex gap-7 justify-center items-center text-lg font-bold">
+    <div className="flex justify-between items-center bg- py-3 px-10 md:px-10 lg:px-20 shadow-xl sticky top-0 z-10">
+       <Link to="/">
+     <div className="flex items-center  justify-center">
+     <img src="https://img.freepik.com/premium-vector/plant-pot-vector-icon-design-illustration_1174953-71906.jpg?uid=R91079514&ga=GA1.1.1302518135.1720608685&semt=ais_user" className="w-[70px] lg:w-[80px]" />
+     <h2 className="font-bold mt-6 text-xl lg:text-2xl text-[#1B3048]">PlantPioneers</h2>
+     </div>
+     </Link>
+      <div className="hidden md:hidden lg:flex gap-7 justify-center items-center text-lg ">
         <NavLink to={"/"}>Home</NavLink>
-        <NavLink to={"/products"}>Products</NavLink>
         <NavLink to={"/management"}>Management</NavLink>
+        <NavLink to={"/products"}>Products</NavLink>
         <NavLink to={"/cart"}>
-          <Badge content="5">
-            <HiOutlineShoppingBag size={"30"} />
+          <Badge content="1">
+            <CiShoppingCart size={"30"} />
           </Badge>
         </NavLink>
       </div>
 
-      <div className="flex md:flex lg:hidden">
-        <GiHamburgerMenu
-          size={"20"}
+      <div className="flex md:flex lg:hidden font-semibold">
+        <IoIosMenu
+          size={"24"}
           className="cursor-pointer"
           onClick={openDrawer}
         />
@@ -37,34 +40,39 @@ const Navbar = () => {
       <Drawer
         open={open}
         onClose={closeDrawer}
-        className="p-4 w-[180px]"
+        className="mt-32 p-4 w-[400px]"
         placeholder={undefined}
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
       >
-        <div className="mb-6 flex flex-col items-center justify-between">
-          <img src={""} className="w-[120px] mb-7" />
+        <div className="mb-6 flex flex-col  justify-between">
+        <Link to="/">
+        <div className="flex items-center  mb-6">
+     <img src="https://img.freepik.com/premium-vector/plant-pot-vector-icon-design-illustration_1174953-71906.jpg?uid=R91079514&ga=GA1.1.1302518135.1720608685&semt=ais_user" className="w-[70px] lg:w-[80px]" />
+     <h2 className="font-bold mt-6 text-xl lg:text-2xl text-[#1B3048]">PlantPioneers</h2>
+     </div>
+        </Link>
 
-          <div className="flex flex-col gap-4 text-center">
-            <NavLink onClick={closeDrawer} to={"/"}>
+          <div className="flex flex-col gap-4 ml-5">
+            <NavLink className="hover:bg-blue-gray-50 p-2" onClick={closeDrawer} to={"/"}>
               Home
             </NavLink>
-
-            <NavLink onClick={closeDrawer} to={"/products"}>
-              Products
-            </NavLink>
-
-            <NavLink onClick={closeDrawer} to={"/management"}>
+            <NavLink className="hover:bg-blue-gray-50 p-2" onClick={closeDrawer} to={"/management"}>
               Management
             </NavLink>
-
-            <NavLink onClick={closeDrawer} to={"/cart"}>
-              <Badge content="5">
-                <HiOutlineShoppingBag size={"30"} />
+            <NavLink className="hover:bg-blue-gray-50 p-2" onClick={closeDrawer} to={"/products"}>
+              Products
+            </NavLink>
+            <NavLink className="hover:bg-blue-gray-50 p-2" onClick={closeDrawer} to={"/cart"}>
+              <Badge content="1">
+                <CiShoppingCart  size={"30"} />
               </Badge>
             </NavLink>
+           <p className="mt-48 text-[12px]"> All rights reserved The Tree Center 2024</p>
           </div>
+          
         </div>
+        
       </Drawer>
     </div>
   );
