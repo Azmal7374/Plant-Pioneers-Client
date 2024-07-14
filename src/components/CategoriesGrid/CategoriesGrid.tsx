28daacdf51aac709dd5b32f578b67588a3a02d67
Azmal <azmalgazi8@gmail.com>
2024-07-14 10:23:51 +0600

    create store and hook

--- src/components/CategoriesGrid/CategoriesGrid.tsx
+++ src/components/CategoriesGrid/CategoriesGrid.tsx
@@ -1,5 +1,3 @@
-
-
 const CategoriesGrid = () => {
   return (
     <div>
@@ -16,72 +14,72 @@ const CategoriesGrid = () => {
           <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
             <a
               href="#"
-              className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+              className="group relative flex h-48 items-center justify-center overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
             >
               <img
                 src="https://www.thetreecenter.com/c/uploads/rhodendron-chionoides-1-450x450.webp"
                 loading="lazy"
-                alt="Photo by Minh Pham"
+                alt="Azmal"
                 className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
               />
 
               <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-              <span className="relative bg-[#1B3048] p-2 rounded-lg ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
+              <span className="relative bg-[#1B3048] p-2 rounded-lg text-sm text-white md:text-lg">
                 Insite Plants
               </span>
             </a>
 
             <a
               href="#"
-              className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
-            >
-              <img
-                src="https://www.thetreecenter.com/c/uploads/2021/05/perennials-450x450.png"
-                loading="lazy"
-                alt="Photo by Magicle"
-                className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-              />
-
-              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
-
-              <span className="relative ml-4 bg-[#1B3048] p-2 rounded-lg mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-                Outside Plants
-              </span>
-            </a>
-
-            <a
-              href="#"
-              className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
-            >
-              <img
-                src="https://www.thetreecenter.com/c/uploads/little-lime-hydrangea-1-340x453.webp"
-                loading="lazy"
-                alt="Photo by Martin Sanchez"
-                className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-              />
-
-              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
-
-              <span className="relative ml-4 bg-[#1B3048] p-2 rounded-lg mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-                Fruit Plants
-              </span>
-            </a>
-
-            <a
-              href="#"
-              className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+              className="group relative flex h-48 items-center justify-center overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
             >
               <img
                 src="https://www.thetreecenter.com/c/uploads/2019/08/Summer-Crush-Hydrangea-340x453.png"
                 loading="lazy"
-                alt="Photo by Lorenzo Herrera"
+                alt="Azmal"
                 className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
               />
 
               <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-              <span className="relative ml-4 bg-[#1B3048] p-2 rounded-lg mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
+              <span className="relative bg-[#1B3048] p-2 rounded-lg text-sm text-white md:text-lg">
+               Outsite Plants
+              </span>
+            </a>
+
+            <a
+              href="#"
+              className="group relative flex h-48 items-center justify-center overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+            >
+              <img
+                src="https://www.thetreecenter.com/c/uploads/2014/06/Endless_Summer_Hydrangea_5G_01-340x453.png"
+                loading="lazy"
+                alt="Azmal"
+                className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+              />
+
+              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+              <span className="relative bg-[#1B3048] p-2 rounded-lg text-sm text-white md:text-lg">
+                Fruits Plants
+              </span>
+            </a>
+
+            <a
+              href="#"
+              className="group relative flex h-48 items-center justify-center overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+            >
+              <img
+                src="https://www.thetreecenter.com/c/uploads/little-lime-hydrangea-1-340x453.webp"
+                loading="lazy"
+                alt="Azmal"
+                className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+              />
+
+              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+              <span className="relative bg-[#1B3048] p-2 rounded-lg text-sm text-white md:text-lg">
                 Flower Plants
               </span>
             </a>
